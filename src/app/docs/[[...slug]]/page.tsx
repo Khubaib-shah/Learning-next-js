e03454944e9a15cd9e98e9751eccc76eee9eeba7
khubaib shah <bewithbaacha@gmail.com>
2025-01-30 18:29:25 +0500

    feat: implement dynamic documentation pages with routing based on slug parameters

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -3,14 +3,13 @@ const Docs = async ({ params }: { params: Promise<{ slug: string[] }> }) => {
   if (slug?.length === 2) {
     return (
       <h1>
-        viewing docs for feature {slug[0]} and concept {slug[1]}
+        viewing doc for {slug[0]} and concept {slug[1]}{" "}
       </h1>
     );
   }
   if (slug?.length === 1) {
     return <h1>viewing doc for {slug[0]}</h1>;
   }
-  return <div>Docs {slug}</div>;
+  return <h1>Docs Home page</h1>;
 };
-
 export default Docs;
